11824d199a8f4ebc5aab23894fb111c500dd1f4e
dethstrobe <dethstrobe@gmail.com>
2019-10-25 21:36:42 -0700

    rendering a grid

--- src/board/renderBoard.ts
+++ src/board/renderBoard.ts
@@ -1,6 +1,13 @@
 export const renderBoard = (ctx: CanvasRenderingContext2D) => {
-  console.log(ctx)
+  const scale = 100
+  const offsetX = 1 * scale
+  const offsetY = 1 * scale
   ctx.beginPath()
-  ctx.rect(20, 20, 150, 100)
+  for (let x = 0; x < 8; ++x) {
+    for (let y = 0; y < 8; ++y) {
+      ctx.rect(x * scale + offsetX, y * scale + offsetY, scale, scale)
+    }
+  }
+
   ctx.stroke()
 }

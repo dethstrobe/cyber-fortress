a55f3e00dc48a0735b3feb74ebce84c05c915fd1
dethstrobe <dethstrobe@gmail.com>
2019-11-01 17:20:13 -0700

    moved move tests in to their own file

--- src/reducers/game.reducer.test.ts
+++ src/reducers/game.reducer.test.ts
@@ -1,55 +1,6 @@
 import gameReducer from "./game.reducer"
-import { MOVE_ACTIONS } from "./move"
 
 describe("Game Reducer", () => {
-  describe("move up", () => {
-    it("should decrease the Y value", () => {
-      const actual = gameReducer(
-        { player: { x: 2, y: 3 } },
-        {
-          type: MOVE_ACTIONS.up,
-        },
-      )
-
-      expect(actual.player).toEqual({ x: 2, y: 2 })
-    })
-  })
-
-  describe("move down", () => {
-    it("should increase the Y value", () => {
-      const actual = gameReducer(undefined, {
-        type: MOVE_ACTIONS.down,
-      })
-
-      expect(actual.player).toEqual({ x: 0, y: 1 })
-    })
-  })
-  describe("move left", () => {
-    it("should decrease the X value", () => {
-      const actual = gameReducer(
-        { player: { x: 2, y: 3 } },
-        {
-          type: MOVE_ACTIONS.left,
-        },
-      )
-
-      expect(actual.player).toEqual({ x: 1, y: 3 })
-    })
-  })
-
-  describe("move right", () => {
-    it("should decrease the X value", () => {
-      const actual = gameReducer(
-        { player: { x: 2, y: 3 } },
-        {
-          type: MOVE_ACTIONS.right,
-        },
-      )
-
-      expect(actual.player).toEqual({ x: 3, y: 3 })
-    })
-  })
-
   it("should return state if an invalid action is passed", () => {
     const state = { player: { x: 1, y: 2 } }
 

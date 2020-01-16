9dfca5ba327bd8372e9cde076942052dd41425bf
dethstrobe <dethstrobe@gmail.com>
2020-01-15 21:27:30 -0800

    add test to make bad guys turn around

--- src/reducers/Enemies/Enemies.test.ts
+++ src/reducers/Enemies/Enemies.test.ts
@@ -1,12 +1,18 @@
 import { enemyReducer } from "."
 import { State, _ } from "../game.reducer"
-
-const state = new State()
+import { GameMap, EnemyLocation } from "../types"
 
 describe("enemy reducer", () => {
+  const setup = (map?: GameMap, enemyLocations?: EnemyLocation): State => {
+    const state = new State()
+    if (map) state.map = map
+    if (enemyLocations) state.enemyLocations = enemyLocations
+
+    return state
+  }
   describe("enemy movement", () => {
     it("enemies should move the direction that want to", () => {
-      const newState = enemyReducer(state)
+      const newState = enemyReducer(setup())
 
       expect(newState.enemyLocations).toEqual([
         [_, _, _, _, _, _, _, _, _, _],
@@ -19,6 +25,32 @@ describe("enemy reducer", () => {
         [_, _, _, _, _, _, _, _, _, _],
         [_, _, _, _, _, _, _, _, _, _],
         [_, _, _, _, _, _, _, _, _, _],
+      ])
+    })
+
+    it("should head the opposite direction once it hits the edge of the map", () => {
+      const newState = enemyReducer(
+        setup(
+          [
+            ["O", "O", "O", "X"],
+            ["O", "O", "O", "X"],
+            ["O", "O", "O", "X"],
+            ["O", "O", "O", "O"],
+          ],
+          [
+            [_, 1, _, _],
+            [_, _, _, _],
+            [_, _, _, _],
+            [_, _, 0, _],
+          ],
+        ),
+      )
+
+      expect(newState.enemyLocations).toEqual([
+        [_, _, _, _],
+        [_, 1, _, _],
+        [_, _, 0, _],
+        [_, _, _, _],
       ])
     })
   })
